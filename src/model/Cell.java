9e46114c2c95cac928159dec67dac569c6432f56
Vintache Jean <jean.vintache@etu.utc.fr>
2016-05-21 18:14:39 +0200

    remove useless statement

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -3,6 +3,8 @@ package model;
 import sim.engine.SimState;
 import sim.engine.Steppable;
 import sim.engine.Stoppable;
+import sim.field.grid.Grid2D;
+import sim.field.grid.ObjectGrid2D;
 import sim.util.Int2D;
 
 public class Cell implements Steppable {
@@ -14,12 +16,16 @@ public class Cell implements Steppable {
     public Cell(Int2D location, CellState state) {
         this.location = location;
         this.state = state;
-        this.stoppable = stoppable;
     }
 
     @Override
     public void step(SimState simState) {
         Beings beings = (Beings) simState;
+
+        ObjectGrid2D grid = beings.getGrid();
+
+        grid.getMooreNeighbors(location.x, location.y, 1, Grid2D.TOROIDAL, false);
+
     }
 
     public Stoppable getStoppable() {
